125bcee8d7d6c169973eaef62a5fac4db28772f6
Matthias Kuhs <matthiku@gmail.com>
2016-02-18 14:34:07 +0000

    resolved issue #31

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,37 +14,61 @@
 
 
                 <div class="card card-block text-xs-center">
-                  <p>Welcome, <strong>{{ Auth::user()->first_name }}</strong>, to </p>
+                    <p>Welcome, <strong>{{ Auth::user()->first_name }}</strong>, to </p>
 
-                  <h3 class="card-title">
+                    <h3 class="card-title">
                         ch-SPOT, the <span class="text-primary">ch</span>urch-<span class="text-primary">S</span>ervice 
                         <span class="text-primary">P</span>lanning <span class="text-primary">O</span>nline <span class="text-primary">T</span>ool
-                  </h3>
-                  for
-                  <h4>
-                    <img src="{{ url('images/churchLogo.png') }}" height="30" width="40">
-                    {{ env('CHURCH_NAME') }}
-                  </h4>
+                    </h3>
+                    for
+                    <h4>
+                        <img src="{{ url('images/churchLogo.png') }}" height="30" width="40">
+                        {{ env('CHURCH_NAME') }}
+                    </h4>
 
-                  <hr>
+                    <hr>
 
-                  <p class="card-text">
-                    <a data-toggle="tooltip" title="Plans where you are leader or teacher"
-                       href="{{ url('cspot/plans') }}" 
-                       class="btn btn-lg btn-info md-full">
-                           Your Service Plans</a> &nbsp; &nbsp; 
-                    <a data-toggle="tooltip" title="All upcoming service plans"
-                        href="{{ url('cspot/plans/future') }}" 
-                        class="btn btn-lg btn-primary md-full">
-                            Upcoming Service Plans</a> &nbsp;  &nbsp; 
-                    <a data-toggle="tooltip" title="Go directly to next Sunday's Service Plan"
-                        href="{{ url('cspot/plans/next') }}" 
-                        class="btn btn-lg btn-success md-full">
-                            Next Sunday's Plan</a>
-                  </p>
+
+                    <p class="card-text">
+
+                        <span class="btn btn-lg btn-info md-full">
+                            <a href="{{ url('cspot/plans') }}">
+                                Your Service Plans
+                                <a href="#" data-container="body" data-toggle="tooltip" data-placement="left" class="pull-xs-right" 
+                                        title="Show plans where you are leader or teacher">
+                                    &nbsp; <i class="fa fa-question-circle"></i></a>
+                            </a>
+                        </span>
+                        &nbsp; 
+                        &nbsp; 
+                        <span class="btn btn-lg btn-primary md-full">
+                            <a href="{{ url('cspot/plans/future') }}" class="bg-primary">
+                                Upcoming Service Plans
+                                <a href="#" data-container="body" data-toggle="tooltip" data-placement="left" class="pull-xs-right" 
+                                        title="Show all upcoming Service Plans">
+                                    &nbsp; <i class="fa fa-question-circle bg-primary"></i></a>
+                            </a>
+                        </span>
+                        &nbsp;
+                        &nbsp; 
+                        <span class="btn btn-lg btn-success md-full">
+                            <a href="{{ url('cspot/plans/next') }}">
+                                Next Sunday's Plan
+                                <a href="#" data-container="body" data-toggle="tooltip" data-placement="left" class="pull-xs-right" 
+                                        title="Go directly to next Sunday's Service Plan">
+                                    &nbsp; <i class="fa fa-question-circle"></i></a>
+                            </a>
+                        </span>
+
+                    </p>
+
+
                 </div>
+
   
                 @include('help')
+
+
             </div>
         </div>
     </div>
