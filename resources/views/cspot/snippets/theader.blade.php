49ce4ebd926168991a526003ff925f2fa28fa3cb
Matthias Kuhs <matthiku@gmail.com>
2016-04-19 15:14:39 +0100

    resolved issue #70 sorting and filtering

---
+++ resources/views/cspot/snippets/theader.blade.php
@@ -0,0 +1,26 @@
+
+    <th class="{{ $thclass }}">
+
+        <span class="link" onclick="reloadListOrderBy('{{ $thfname }}')" data-toggle="tooltip" title="Sort list by {{ ucfirst($thdisp) }}">
+            {{ ucfirst($thdisp) }}
+            <i class="fa fa-sort 
+                @if ( Request::has('orderby') && Request::get('orderby')==$thfname )
+                    text-info
+                @endif
+                "> </i>
+        </span>
+
+        @if ( $thsort )
+        <span id="{{ $thfname }}-search" class="link m-l-1" onclick="showFilterField('{{ $thfname }}')" data-toggle="tooltip" 
+            @if ( Request::has('filterby') && Request::get('filterby')==$thfname )
+                    title="Clear filter">
+                Filter: <span class="bg-info">&nbsp;{{ Request::get('filtervalue') }} </span>
+                <i id="filter-{{ $thfname }}-clear" class="fa fa-close"> </i>
+            @else
+                    title="Search within {{ $thdisp }}">
+                <i id="filter-{{ $thfname }}-show" class="fa fa-search"> </i>
+            @endif
+        </span>
+        @endif
+
+    </th>
