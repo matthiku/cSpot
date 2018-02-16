51a3cf963bf9cdfad5368427a216ec06bc45ceab
Matthias Kuhs <matthiku@users.noreply.github.com>
2018-02-16 11:31:03 +0000

    Update 2016_01_19_111642_create-roles.php
    adding "for_events" field

--- database/migrations/2016_01_19_111642_create-roles.php
+++ database/migrations/2016_01_19_111642_create-roles.php
@@ -20,18 +20,19 @@ class CreateRoles extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->text('name');
+            $table->boolean('for_events')->default(true);
             $table->timestamps();
         });
         
         // insert default access rights roles 1=admin, 2=editor, 3=author!
-        DB::table('roles')->insert(['name'=>'administrator']);
-        DB::table('roles')->insert(['name'=>'editor']);
-        DB::table('roles')->insert(['name'=>'author']);
+        DB::table('roles')->insert(['name'=>'administrator', 'for_events'=>0]);
+        DB::table('roles')->insert(['name'=>'editor', 'for_events'=>0]);
+        DB::table('roles')->insert(['name'=>'author', 'for_events'=>0]);
         // insert real life roles
         DB::table('roles')->insert(['name'=>'teacher']);
         DB::table('roles')->insert(['name'=>'leader']);
-        DB::table('roles')->insert(['name'=>'retired']);
-        DB::table('roles')->insert(['name'=>'user']);
+        DB::table('roles')->insert(['name'=>'retired', 'for_events'=>0]);
+        DB::table('roles')->insert(['name'=>'user', 'for_events'=>0]);
     }
 
     /**
